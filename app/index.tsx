8372ba21bcdea550c0515ce66a89298d03fdf6b8
khabzox <152230716+khabzox@users.noreply.github.com>
2025-06-20 01:43:40 +0100

    Update dependencies and refactor app structure
    - Updated `@react-navigation/native` to version 7.1.13 and added `@react-navigation/native-stack` and `@supabase/supabase-js` to dependencies.
    - Added `expo-file-system`, `isomorphic-git`, `nativewind`, and `zustand` to dependencies.
    - Updated `typescript`, `eslint`, and added `prettier-plugin-tailwindcss` and `tailwindcss` to devDependencies.
    - Refactored layout components by removing unused imports and simplifying the structure.
    - Removed the old tab layout and explore screen components, replacing them with a simplified layout.
    - Updated TypeScript configuration to include `nativewind-env.d.ts` and set JSX to `react-native`.

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500 p-10">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
